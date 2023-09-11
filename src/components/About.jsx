15eb875cef215713938e523a5a6560237cb50bbd
Arshya Soleimani <arrshya@gmail.com>
2023-09-10 22:25:22 -0600

    Fix spelling error, reformat names

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = (props) => {
     <div className='about-container container d-flex flex-column justify-content-center align-items-center' id="aboutId">
         <h2 className='text-dark-emphasis p-4'>My name is <strong>Arshya</strong></h2>
         <h4 className='text-dark-emphasis p-4'>I’m a <strong>software developer</strong></h4>
-        <h4 className='text-dark-emphasis p-4'>My educational background is in <strong>mathematics</strong>, <strong>statistics</strong> and <strong>software devlopment</strong></h4>
+        <h4 className='text-dark-emphasis p-4'>My educational background is in <strong>mathematics</strong>, <strong>statistics</strong> and <strong>software development</strong></h4>
         <h4 className='text-dark-emphasis p-4'>Check out my skills and projects bellow! </h4>
         <button className="btn btn-link" onClick={() => {props.scrollToElement('skillsId')}} ><svg className="ms-1 icon-arrow" xmlns="http://www.w3.org/2000/svg" height="0.875em" viewBox="0 0 384 512"><path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></button>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const ProjectsOne = (props) => {
         <div className='project-one container-fluid d-flex flex-row align-items-center p-4' >
           <img className="image border border-dark border-2" src='/images/fitness.png' alt=""/>
           <div className="container-fluid d-flex flex-column justify-content-center align-items-center">
-            <h4 className="description text-dark-emphasis p-2 "><strong>Fitness Planner</strong></h4>
+            <h4 className="description text-dark-emphasis p-2 "><strong>AI Fitness Planner</strong></h4>
             <h6 className="text-dark-emphasis p-2 ">Workout planning with the help of AI</h6>
             <a href="https://github.com/Arshya-S/Fitness-Planner" target="_blank" rel="noopener noreferrer" className="btn btn-dark ms-2 p-2" >Github</a>
           </div>
@@ -14,7 +14,7 @@ const ProjectsOne = (props) => {
 
         <div className='project-two container-fluid d-flex flex-row align-items-center p-4' >
           <div className="container-fluid d-flex flex-column justify-content-center align-items-center">
-            <h4 className="description text-dark-emphasis p-2 "><strong>Smart TODO List</strong></h4>
+            <h4 className="description text-dark-emphasis p-2 "><strong>Smart To-Do List</strong></h4>
             <h6 className="text-dark-emphasis p-2 ">Auto-categorizing TODO list</h6>
             <a href="https://github.com/Arshya-S/Smart-TODO-List" target="_blank" rel="noopener noreferrer" className="btn btn-dark ms-2 p-2" >Github</a>
           </div>
@@ -32,7 +32,7 @@ const ProjectsOne = (props) => {
 
         <div className='project-four container-fluid d-flex flex-row align-items-center p-4'>
           <div className="container-fluid d-flex flex-column justify-content-center align-items-center">
-            <h4 className="text-dark-emphasis p-2 "><strong>Photolabs</strong></h4>
+            <h4 className="text-dark-emphasis p-2 "><strong>Photo Share</strong></h4>
             <h6 className="description text-dark-emphasis p-2 ">Users manage their photos and interact with other's</h6>
             <a href="https://github.com/Arshya-S/Photolabs" target="_blank" rel="noopener noreferrer" className="btn btn-dark ms-2 p-2" >Github</a>
           </div>
